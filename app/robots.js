3ea5e8f11e2ed0585882695a98afa324f417bb59
thexpertguidecom-star <thexpertguide.com@gmail.com>
2025-12-24 04:44:10 +0530

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,17 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        crawlDelay: 0,
+      },
+    ],
+    sitemap: 'https://thexpertguide.vercel.app/sitemap.xml',
+  }
+}
